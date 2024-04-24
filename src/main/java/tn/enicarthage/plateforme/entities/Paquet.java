7ddad1dbc286885f77d7bd13ff65b06b4d043352
Hachem Mastouri <hachemprince12@gmail.com>
2024-04-24 15:35:09 +0100

    attribut date pour paquet

--- src/main/java/tn/enicarthage/plateforme/entities/Paquet.java
+++ src/main/java/tn/enicarthage/plateforme/entities/Paquet.java
@@ -1,8 +1,10 @@
 package tn.enicarthage.plateforme.entities;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
@@ -32,5 +34,6 @@ public class Paquet implements Serializable  {
 
 	@ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
 	Matiere examen;
-
+	@JsonFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dateCopie;
 }
